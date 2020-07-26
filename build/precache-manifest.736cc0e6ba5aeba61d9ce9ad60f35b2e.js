9bab81afdc5bcb5d6caa61b90dcd3c34ad8485c6
Alexander Kagadovsky <alexanderkagadovsky@coherentsolutions.com>
2020-07-26 23:18:36 +0300

    init

---
+++ build/precache-manifest.736cc0e6ba5aeba61d9ce9ad60f35b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c80f6521df85a3c6436589139b7ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30372102c141601e8753",
+    "url": "/static/css/2.c4587cfe.chunk.css"
+  },
+  {
+    "revision": "e8d6f729ec6ebe90512d",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "30372102c141601e8753",
+    "url": "/static/js/2.b5d01e3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b5d01e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d6f729ec6ebe90512d",
+    "url": "/static/js/main.f5287dc9.chunk.js"
+  },
+  {
+    "revision": "6a441b3ced69f69cbbb4",
+    "url": "/static/js/runtime-main.02f32b56.js"
+  }
+]);
